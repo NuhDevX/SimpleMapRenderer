b0f7ee25cb60d773411d7d35f83c6039445efb2a
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-11-01 22:00:58 +0700

    Add ItemPlus class for filled map handling
    Implement ItemPlus class with setup method for filled map registration.

---
+++ src/alvin0319/SimpleMapRenderer/item/ItemPlus.php
@@ -0,0 +1,14 @@
+<?php
+
+alvin0319\SimpleMapRenderer\item;
+
+use pocketmine\utils\CloningRegistryTrait;
+uae pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemTypeIds;
+class ItemPlus {
+  use CloningRegistryTrait;
+
+  protected static function setup(): void {
+    self::_registryRegister('filled_map', new FilledMap(new ItemIdentifier(ItemTypeIds::newId()), "Filled Map"));   
+  }
+}
